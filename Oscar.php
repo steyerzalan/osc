d6ca7184240927a9edfe96f40aaec5dd833db0ac
steyerzalan <steyerzalan@gmail.com>
2025-03-10 11:50:24 +0100

    dbvezerlo kétféle(PDO,mysqli) módon elkészült

--- Oscar.php
+++ Oscar.php
@@ -1,11 +1,11 @@
 <?php 
-require_once("dbvezerlopdo.php");
+require_once("dbvezerlo.php");
 
 class Oscar {
     private $oscars=[];
     public function __construct(){
         $query = "SELECT m_ID, title, m_desc, pic FROM movie";
-        $dbvez = new DBController();
+        $dbvez = new DBVezerlo();
         $this->oscars = $dbvez->executeSelectQuery($query);
         $dbvez->closeDB();
     }
@@ -15,7 +15,7 @@ class Oscar {
     public function getOscarsById($OscarId){
         $query = "SELECT m_ID, title, m_desc, pic 
         FROM movie WHERE , m_ID = ".$OscarId;
-        $dbvez = new DBController();
+        $dbvez = new DBVezerlo();
         $this->oscars = $dbvez->executeSelectQuery($query);
         $dbvez->closeDB();
         return $this->oscars;
@@ -25,7 +25,7 @@ class Oscar {
         FROM movie
         inner join movie_type on movie_type.mt_ID=movie.m_type
         where movie_type.Mt_name= '".$Mt_name."'";
-        $dbvez = new DBController();
+        $dbvez = new DBVezerlo();
         $this->oscars=$dbvez->executeSelectQuery($query);
         $dbvez->closeDB();
         return $this->oscars;
